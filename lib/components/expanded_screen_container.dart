f9927624a7316767023ae77cccf9a387359b36b2
toxirjon96 <tatu.toxirjon@gmail.com>
2023-08-17 21:18:29 +0500

    last modification

--- lib/components/expanded_screen_container.dart
+++ lib/components/expanded_screen_container.dart
@@ -25,43 +25,45 @@ class ExpandedScreenContainer extends StatelessWidget {
       child: Container(
         color: screenBackgroundColor,
         alignment: Alignment.bottomCenter,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    countingText,
-                    style: GoogleFonts.lato(
-                      color: textColor,
-                      fontSize: 40,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      countingText,
+                      style: GoogleFonts.lato(
+                        color: textColor,
+                        fontSize: 40,
+                      ),
                     ),
                   ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    result,
-                    style: GoogleFonts.lato(
-                      color: textColor,
-                      fontSize: 40,
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      result,
+                      style: GoogleFonts.lato(
+                        color: textColor,
+                        fontSize: 40,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Divider(
-                thickness: 5,
-                indent: (screenWidth - 80) / 2,
-                endIndent: (screenWidth - 80) / 2,
-                color: textColor!.withOpacity(0.3),
+                ],
               ),
-            ),
-          ],
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Divider(
+                  thickness: 5,
+                  indent: (screenWidth - 80) / 2,
+                  endIndent: (screenWidth - 80) / 2,
+                  color: textColor!.withOpacity(0.3),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
